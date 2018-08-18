e767e296f2d799723530533adaab380c1b79dfcd
danmj <daniel.jung@stern.nyu.edu>
2018-08-18 16:10:20 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,1 +1,13 @@
 class Hash
+    array =[]
+    idx = 0
+    while idx < arguments.size
+    each {|k, v|
+      if v == arguments[idx]
+        array << k
+      end
+      }
+      idx += 1
+    end
+    array
+end
